f524b37c09e1f113f17616be7b18dc13998bba7e
Mykola1612 <fermer2007pavlovich@gmail.com>
2026-05-04 02:31:49 +0200

    Summary

--- src/components/sections/hairSystems/HairSystems.tsx
+++ src/components/sections/hairSystems/HairSystems.tsx
@@ -140,8 +140,7 @@ export const HairSystems = () => {
           </div>
 
           <Button
-            className="uppercase [@media(hover:hover)_and_(pointer:fine)]:hover:bg-accentHover
-              [@media(hover:hover)_and_(pointer:fine)]:hover:text-primary/80
+            className="uppercase hover:bg-accentHover hover:text-primary/80
               focus-visible:bg-accentHover focus-visible:text-primary/80
               focus-visible:ring-offset-foreground active:bg-accentHover active:text-primary/80"
           >
